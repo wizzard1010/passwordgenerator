78d79e4c25c09442d5090a5a9665d11bafca596d
wizzard1010 <116697208+wizzard1010@users.noreply.github.com>
2023-08-08 16:30:33 +0630

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,30 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+for i in range(len(letters)):
+    select_letter = random.sample(letters,nr_letters)
+    
+    
+for j in range(len(numbers)):
+    select_numbers = random.sample(numbers,nr_numbers)
+    
+    
+for k in range(len(symbols)):
+    select_symbol = random.sample(symbols,nr_symbols)
+    
+    
+total = select_letter + select_numbers + select_symbol
+random.shuffle(total)
+result = ''.join(total)
+print(result)
+
+    
